1524e5de0d66cd5435a108f8529a805792e0e21e
Ceren Susuz <ceren_susuz@epam.com>
2025-07-21 10:45:23 +0300

    refactor: extract DecimalPlaces to shared FeedValidationUtils helper

---
+++ Helpers/FeedValidationUtils.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeedManagerApp.Helpers
+{
+    public static class FeedValidationUtils
+    {
+        public static int DecimalPlaces(decimal number)
+        {
+            var str = number.ToString(CultureInfo.InvariantCulture);
+            var dot = str.IndexOf('.');
+
+            return dot < 0 ? 0 : str.Length - dot - 1;
+        }
+    }
+}
